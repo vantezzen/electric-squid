1fa1c168bdf37627cb3837dc914a28fb36413c5b
Bennett <hello@vantezzen.io>
2022-05-18 16:19:35 +0200

    Fix dependency patching

--- components/client/src/index.js
+++ components/client/src/index.js
@@ -5,4 +5,4 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
